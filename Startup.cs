e9384f42451193e849846e49e173011f6d01733e
itahirov <38325195+itahirov@users.noreply.github.com>
2021-06-03 17:24:49 -0400

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace NewRelicDemo_NN_01
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World 6!");
+                    await context.Response.WriteAsync("Hello World 8!");
                 });
                 endpoints.MapControllerRoute(
 
